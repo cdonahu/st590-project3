cb93421af219bd2d16220eb49929e4b51b26a3cc
Natalia Peshekhodko <nataliya.peshekhodko@slalom.com>
2022-07-02 22:24:11 -0400

    Some clean up and reorginizing

--- Project3.py
+++ Project3.py
@@ -1,19 +1,25 @@
+#######################################################
+# ST - 590
+# Project 3
+# Performed by Claudia Donahue and Nataliya Peshekhodko
+#######################################################
+
 import pandas as pd
 import math
 import time
 
+#######################################################
+# Set up for Creating Files
+#######################################################
 all_accs = pd.read_csv("data/all_accelerometer_data_pids_13.csv")
-
-
 SA0297_df = all_accs.loc[all_accs.pid=='SA0297']
 PC6771_df = all_accs.loc[all_accs.pid=='PC6771']
 
 position = 0
 step=500
 
-print ('max number of interations ' + str (math.ceil(max(SA0297_df.shape[0], PC6771_df.shape[0])/500)))
+print ('Max number of iterations ' + str (math.ceil(max(SA0297_df.shape[0], PC6771_df.shape[0])/500)))
 
-#for i in range (0, math.floor(min(SA0297_df.shape[0], PC6771_df.shape[0])/500)):
 for i in range (0, math.ceil(max(SA0297_df.shape[0], PC6771_df.shape[0])/500)):
     
     print ("Position " + str(position))
@@ -38,6 +44,7 @@ for i in range (0, math.ceil(max(SA0297_df.shape[0], PC6771_df.shape[0])/500)):
         
     if not output_PC6771.empty:  
         output_PC6771.to_csv("pc6771_csv_files/pc6771_" + str(i) + ".csv", index = False, header = False)
+        
     time.sleep(25)
     position = position+step
     
@@ -45,7 +52,9 @@ for i in range (0, math.ceil(max(SA0297_df.shape[0], PC6771_df.shape[0])/500)):
     output_SA0297 = pd.DataFrame ()
     
     
-    
+#######################################################
+# Reading a Stream
+#######################################################
 from pyspark.sql.types import StructType
 
 myschema = StructType() \
@@ -55,27 +64,35 @@ myschema = StructType() \
 .add("y", "float") \
 .add("z", "float")
 
-
 df1 = spark \
 .readStream \
 .schema(myschema) \
-.csv("sa0297_csv_files/")  
-
-agg_df1 = df1.select('time', 'pid', (pow( (df1['x']*df1['x'] + df1['y']*df1['y'] + df1['z']*df1['z']), 0.5)))
-q1 = agg_df1.writeStream \
-.outputMode("append") \
-.format("csv") \
-.option("path", "data/output_SA0297/") \
-.option("checkpointLocation","checkpoints") \
-.start()
-
+.csv("sa0297_csv_files/")
 
 df2 = spark \
 .readStream \
 .schema(myschema) \
 .csv("pc6771_csv_files/") 
 
+#######################################################
+# Transform/Aggregation Step
+#######################################################
+
+agg_df1 = df1.select('time', 'pid', (pow( (df1['x']*df1['x'] + df1['y']*df1['y'] + df1['z']*df1['z']), 0.5)))
+
 agg_df2 = df2.select('time', 'pid', (pow( (df2['x']*df2['x'] + df2['y']*df2['y'] + df2['z']*df2['z']), 0.5)))
+
+
+#######################################################
+# Writing the Streams
+#######################################################
+
+q1 = agg_df1.writeStream \
+.outputMode("append") \
+.format("csv") \
+.option("path", "data/output_SA0297/") \
+.option("checkpointLocation","checkpoints") \
+.start()
 
 q2 = agg_df2.writeStream \
 .outputMode("append") \
@@ -84,19 +101,28 @@ q2 = agg_df2.writeStream \
 .option("checkpointLocation","checkpoints_2") \
 .start()
 
+#######################################################
+# Read in all "part" files
+#######################################################
+allfiles_sa = spark \
+.read.option("header","false") \
+.csv("data/output_SA0297/part-*.csv") 
 
-allfiles = spark.read.option("header","false").csv("data/output_SA0297/part-*.csv") # Output as CSV file
-allfiles \
+allfiles_pc = spark \
+.read.option("header","false") \
+.csv("data/output_PC6771/part-*.csv") 
+
+#######################################################
+# Output to single CSV file
+#######################################################
+allfiles_sa \
 .coalesce(1) \
 .write.format("csv") \
 .option("header", "false") \
 .save("final_output_SA0297/single_csv_file/")
 
-
-allfiles = spark.read.option("header","false").csv("data/output_PC6771/part-*.csv") # Output as CSV file
-allfiles \
+allfiles_pc \
 .coalesce(1) \
 .write.format("csv") \
 .option("header", "false") \
 .save("final_output_PC6771/single_csv_file/")
-
